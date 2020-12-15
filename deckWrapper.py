482a1af943642d1ef40a71330c9d16b0f63d5530
Tim Inzitari <tsi3@zips.uakron.edu>
2020-12-15 18:11:43 -0500

    Convert deckWrapper to NumPy

--- deckWrapper.py
+++ deckWrapper.py
@@ -1,5 +1,7 @@
 from hearthstone import deckstrings
 from hearthstone.enums import FormatType
+from typing import List, Tuple
+import numpy as np
 
 class DeckWrapper:
 
@@ -12,5 +14,11 @@ class DeckWrapper:
 		self.uID = uID
 		self.deckCode = deckString
 		self.deck = deckstrings.Deck().from_deckstring(self.deckCode)
-		self.cardList = self.deck.get_dbf_id_list()
+		self.cardList = []
 		self.classification = "NOCLASSIFY"
+
+		for cardTuple in self.deck.get_dbf_id_list():
+			self.cardList.append(cardTuple[0])
+			if cardTuple[1] == 2:
+				self.cardList.append(cardTuple[0])
+		self.cardList = np.array(self.cardList)
--- test.py
+++ test.py
@@ -1,22 +1,25 @@
+import numpy as np
+
 print("RUNNING TESTS")
 
-
-
+#-------------------------------------------------
 # DECK WRAPPER
 from deckWrapper import DeckWrapper
-
-deck = DeckWrapper("TeamA", 1, 3,"AAECAR8eqAK1A8cDhwSKB9sJ/gz8owOmpQP5rgP7rwP8rwOHsAOiuQOkuQP/ugPXvgPevgPczAOizgOC0APG0QO50gP21gPo4QPy4QOE4gOG4gOP4wPK4wMAAA==")
+from typing import List
+deck = DeckWrapper("TeamA", 1, 3,"AAECAQcIogKKB5uUA9+tA8XAA/bCA5PQA6rSAwtL/wOiBP8HmpQD2a0D6LADuLkDwLkD99QDtt4DAA==")
 
 assert deck.teamName == "TeamA", "Error in deckWrapper Class with teamName"
 assert deck.ingameClass == 1, "Error in deckWrapper Class with inGameClass"
 assert deck.uID == 3, "Error in deckWrapper Class with uID"
-assert deck.deckCode == "AAECAR8eqAK1A8cDhwSKB9sJ/gz8owOmpQP5rgP7rwP8rwOHsAOiuQOkuQP/ugPXvgPevgPczAOizgOC0APG0QO50gP21gPo4QPy4QOE4gOG4gOP4wPK4wMAAA==", "Error in deckWrapper Class with deckCode"
-assert deck.cardList == [(296, 1), (437, 1), (455, 1), (519, 1), (906, 1), (1243, 1), (1662, 1), (53756, 1), (53926, 1), (55161, 1), (55291, 1), (55292, 1), (55303, 1), (56482, 1), (56484, 1), (56703, 1), (57175, 1), (57182, 1), (58972, 1), (59170, 1), (59394, 1), (59590, 1), (59705, 1), (60278, 1), (61672, 1), (61682, 1), (61700, 1), (61702, 1), (61839, 1), (61898, 1)], "Error in deckWrapper Class when parsing Card List"
+assert deck.deckCode == "AAECAQcIogKKB5uUA9+tA8XAA/bCA5PQA6rSAwtL/wOiBP8HmpQD2a0D6LADuLkDwLkD99QDtt4DAA==", "Error in deckWrapper Class with deckCode"
+testList = [75, 75, 290, 511, 511, 546, 546, 906, 1023, 1023, 51738, 51738, 51739, 55001, 55001, 55007, 55400, 55400, 56504, 56504, 56512, 56512, 57413, 57718, 59411, 59690, 60023, 60023, 61238, 61238]
+assert np.array_equal(testList, deck.cardList), "error in deckWrapper Class when parsing Card List"
+assert len(deck.cardList) == 30, "Error in Card Parsing"
+#for i in testList:
+	#assert deck.cardList[i] == testList[i], "Error in deckWrapper Class when parsing Card List"
 assert deck.classification == "NOCLASSIFY", "Error in deckWrapper with Classification initialization"
 
 print("\tdeckWrapper Class passes all tests")
 
 #END DECK WRAPPER
-
-
-
+#-------------------------------------------------
