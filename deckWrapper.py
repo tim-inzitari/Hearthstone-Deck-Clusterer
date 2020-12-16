69c96c8f0dd9341741f8e797714cdf8bc801fffa
Tim Inzitari <tsi3@zips.uakron.edu>
2020-12-16 12:19:51 -0500

    Fixed Alternate Heros in DeckWrapper

--- deckWrapper.py
+++ deckWrapper.py
@@ -2,15 +2,26 @@ from hearthstone import deckstrings
 from hearthstone.enums import FormatType
 from typing import List, Tuple
 import numpy as np
+import json
+
+#Hearthstone Card JSon needed for class parsing
+# https://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json
+cards_json = 'resources/cards.collectible.json'
+card_dict = {}
+with open(cards_json, encoding="utf-8") as json_file:
+    data = json.load(json_file)
+    for card in data:
+        card_dict[card['dbfId']] = card
 
 class DeckWrapper:
 
+	
+
 	# Constructor
-	def __init__(self, teamName, ingameClass, uID, deckString):
+	def __init__(self, teamName, uID, deckString):
 		self.deck = deckstrings.Deck()
 
 		self.teamName = teamName
-		self.ingameClass = ingameClass
 		self.uID = uID
 		self.deckCode = deckString
 		self.deck = deckstrings.Deck().from_deckstring(self.deckCode)
@@ -21,4 +32,6 @@ class DeckWrapper:
 			self.cardList.append(cardTuple[0])
 			if cardTuple[1] == 2:
 				self.cardList.append(cardTuple[0])
-		self.cardList = np.array(self.cardList)
+
+		self.ingameClass = card_dict[self.deck.heroes[0]]
+		self.ingameClass = self.ingameClass['cardClass'].lower()
--- test.py
+++ test.py
@@ -6,10 +6,12 @@ print("RUNNING TESTS")
 # DECK WRAPPER
 from deckWrapper import DeckWrapper
 from typing import List
-deck = DeckWrapper("TeamA", 1, 3,"AAECAQcIogKKB5uUA9+tA8XAA/bCA5PQA6rSAwtL/wOiBP8HmpQD2a0D6LADuLkDwLkD99QDtt4DAA==")
+
+#test deck is BankYugi's warrior from "Hearthstone World Championships 2020"
+deck = DeckWrapper("TeamA", 3,"AAECAQcIogKKB5uUA9+tA8XAA/bCA5PQA6rSAwtL/wOiBP8HmpQD2a0D6LADuLkDwLkD99QDtt4DAA==")
 
 assert deck.teamName == "TeamA", "Error in deckWrapper Class with teamName"
-assert deck.ingameClass == 1, "Error in deckWrapper Class with inGameClass"
+assert deck.ingameClass == "warrior", "Error in deckWrapper Class with inGameClass"
 assert deck.uID == 3, "Error in deckWrapper Class with uID"
 assert deck.deckCode == "AAECAQcIogKKB5uUA9+tA8XAA/bCA5PQA6rSAwtL/wOiBP8HmpQD2a0D6LADuLkDwLkD99QDtt4DAA==", "Error in deckWrapper Class with deckCode"
 testList = [75, 75, 290, 511, 511, 546, 546, 906, 1023, 1023, 51738, 51738, 51739, 55001, 55001, 55007, 55400, 55400, 56504, 56504, 56512, 56512, 57413, 57718, 59411, 59690, 60023, 60023, 61238, 61238]
@@ -19,7 +21,16 @@ assert len(deck.cardList) == 30, "Error in Card Parsing"
 	#assert deck.cardList[i] == testList[i], "Error in deckWrapper Class when parsing Card List"
 assert deck.classification == "NOCLASSIFY", "Error in deckWrapper with Classification initialization"
 
+
+
+deck = DeckWrapper("MalfTest", 4, "AAECAZICBvIFrqsClL0C+cACws4CmdMCDEBf/gHEBuQItLsCy7wCz7wC3b4CyccCoM0Ch84CAA==")
+assert deck.ingameClass == "druid", "Error in deckWrapper Class when parsing alternate hero portraits"
+
 print("\tdeckWrapper Class passes all tests")
+
+
 
 #END DECK WRAPPER
 #-------------------------------------------------
+
+
