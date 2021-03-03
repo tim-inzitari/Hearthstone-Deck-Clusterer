aac0b677fbd2a80cc09517a7ab8ea270141b4455
Tim-Inzitari <tsi3@zips.uakron.edu>
2021-03-03 18:36:36 +0000

    remove single threaded csv gahterhering

--- CSVs/getCSVs.py
+++ CSVs/getCSVs.py
@@ -5,29 +5,22 @@ import sys
 import os
 import csv
 import datetime
+import multiprocessing
+from multiprocessing.pool import Pool
+from itertools import product
 
 
-getTournyIDs = []
-tournyIDs = []
-data = requests.get("https://majestic.battlefy.com/hearthstone-masters/tournaments?start=2021-01-26T08:00:42.465Z&end=2021-02-01T08:00:42.465Z").json()
-for tourny in data:
-	getTournyIDs.append(tourny['_id'])
-for tourny in getTournyIDs:
-	data = requests.get("https://majestic.battlefy.com/tournaments/{}/".format(tourny)).json()
-	tournyIDs.append(data['stageIDs'][0])
-count = 1
+NUM_PROCESSSES = 20
+START_DATE = "2021-02-20" #YYYY-MM-DD
+END_DATE = "2021-03-02"#YYYY-MM-DD
 
-
-
-
-def parseTournament(stage, tourny):
-	print(tourny)
+def parseTournament(stage, tourny, count):
+	print("Start tourny {} id: {}".format(count+1, tourny))
 	data= requests.get("https://dtmwra1jsgyb0.cloudfront.net/stages/{}/matches?roundNumber=1".format(stage)).json()
 	#print(type(data))
 	#print(type(data[3]))
 
 	#print(data[0]['_id'])
-
 
 	matches = []
 
@@ -38,22 +31,60 @@ def parseTournament(stage, tourny):
 	#print(matches)
 	data = requests.get("https://majestic.battlefy.com/tournaments/{}/matches/{}/deckstrings".format(tourny, matches[4]['_id'])).json()
 	#print(data['top'][1])
-
-	filepath = "csv.csv"
+	
 
 
 	myString= ""
 	for match in matches:
 		data=requests.get("https://majestic.battlefy.com/tournaments/{}/matches/{}/deckstrings".format(tourny, match['_id'])).json()
-		myString+="\n"+"{}_{}".format(count, match['top']['team']['name']).replace("\n", "").replace(",","") + ","+"{}".format(data['top'][0]).replace("\n", "").replace(",","")+","+ "{}".format(data['top'][1]).replace("\n", "").replace(",","")+","+"{}".format(data['top'][2]).replace("\n", "").replace(",","")
+		myString+="\n{},".format(tourny)+"{}_{}".format(count+1, match['top']['team']['name']).replace("\n", "").replace(",","") + ","+"{}".format(data['top'][0]).replace("\n", "").replace(",","")+","+ "{}".format(data['top'][1]).replace("\n", "").replace(",","")+","+"{}".format(data['top'][2]).replace("\n", "").replace(",","")
+		if match['isBye'] =="False":
+			myString+="\n{},".format(tourny)+"{}_{}".format(count+1, match['bottom']['team']['name']).replace("\n", "").replace(",","") + ","+"{}".format(data['bottom'][0]).replace("\n", "").replace(",","")+","+ "{}".format(data['bottom'][1]).replace("\n", "").replace(",","")+","+"{}".format(data['bottom'][2]).replace("\n", "").replace(",","")
 
+	print("End Tourny {}".format(count+1))
+	return myString
+
+#	with open(filepath, "a", newline='\n', encoding='utf-8') as csvfile:
+	#	csvWriter = csv.writer(csvfile, delimiter=",", quotechar='"', quoting = csv.QUOTE_NONE, escapechar='~')
+	#	if os.stat(filepath).st_size == 0:
+			#csvWriter.writerow(['K', 'D', 'D', 'D', 'D'])
+
+		#for match in matches:
+			#data=requests.get("https://majestic.battlefy.com/tournaments/{}/matches/{}/deckstrings".format(tourny, match['_id'])).json()
+			#csvWriter.writerow(["{}_{}".format(count, match['top']['team']['name']).replace("\n", "").replace(",",""), "{}".format(data['top'][0]).replace("\n", "").replace(",",""), "{}".format(data['top'][1]).replace("\n", "").replace(",",""), "{}".format(data['top'][2]).replace("\n", "").replace(",","")])
+			#if match['isBye'] =="False":
+			#	csvWriter.writerow(["{}_{}".format(count,match['bottom']['team']['name']).replace("\n", "").replace(",",""), "{}".format(data['bottom'][0]).replace("\n", "").replace(",",""), "{}".format(data['bottom'][1]).replace("\n", "").replace(",",""), "{}".format(data['bottom'][2]).replace("\n", "").replace(",","")])
+
+
+def main():
+	getTournyIDs = []
+	tournyIDs = []
+	data = requests.get("https://majestic.battlefy.com/hearthstone-masters/tournaments?start={}T08:00:42.465Z&end={}T08:00:42.465Z".format(START_DATE, END_DATE)).json()
+	for tourny in data:
+		getTournyIDs.append(tourny['_id'])
+	num_tourneys = len(getTournyIDs)
+	for tourny in getTournyIDs:
+		data = requests.get("https://majestic.battlefy.com/tournaments/{}/".format(tourny)).json()
+		tournyIDs.append(data['stageIDs'][0])
+	filepath = "csv.csv"
+	print("Parsing {} tournaments using up to {} processes.".format(num_tourneys, NUM_PROCESSSES))
+	p = Pool(processes = NUM_PROCESSSES)
+	returnData = p.starmap(parseTournament, zip(tournyIDs, getTournyIDs, range(num_tourneys)))
+	p.close()
+	p.join()
+
+
+	print("Parsing Complete\nStart CSV file write:")
 	with open(filepath, "a", newline='\n', encoding='utf-8') as csvfile:
-		csvWriter = csv.writer(csvfile, delimiter=",", quotechar='"', quoting = csv.QUOTE_NONE, escapechar='~')
 		if os.stat(filepath).st_size == 0:
-			csvWriter.writerow(['K', 'D', 'D', 'D', 'D'])
+			csvfile.write(",K,D,D,D") #TournyID, Name, Deck, Deck, Deck
+		for processString in returnData:
+			split = processString.split(",")
+			for line in split:
+				csvfile.write("{}".format(line))
+				csvfile.write(",")
+			
 
-		for match in matches:
-			data=requests.get("https://majestic.battlefy.com/tournaments/{}/matches/{}/deckstrings".format(tourny, match['_id'])).json()
-			csvWriter.writerow(["{}_{}".format(count, match['top']['team']['name']).replace("\n", "").replace(",",""), "{}".format(data['top'][0]).replace("\n", "").replace(",",""), "{}".format(data['top'][1]).replace("\n", "").replace(",",""), "{}".format(data['top'][2]).replace("\n", "").replace(",","")])
-			if match['isBye'] =="False":
-				csvWriter.writerow(["{}_{}".format(count,match['bottom']['team']['name']).replace("\n", "").replace(",",""), "{}".format(data['bottom'][0]).replace("\n", "").replace(",",""), "{}".format(data['bottom'][1]).replace("\n", "").replace(",",""), "{}".format(data['bottom'][2]).replace("\n", "").replace(",","")])
+	print("Finish CSV File Write")
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Honors-Project
+# Deck Clusterer   
+
+A python script for converting Hearthstone deck codes from a csv to declarations of their archetypes in csv format.
+
+## Requirements    
+Language that must be installed    
+* Python 3.8+   
+
+Packages that can must be installed   
+This can be done by following instructions in **Setup**          
+* [python-hearthstone](https://github.com/hearthsim/python-hearthstone)
+* MatPlotLib
+* Scikit-learn
+* [python-hearthstone](https://github.com/hearthsim/hsdata)
+* PySimpleGui
+* scikit-learn
+* pandas
+* [Python Imaging Library](https://pillow.readthedocs.io)
+* [backports.csv](https://pypi.python.org/pypi/backports.csv)
+* [Requests](http://docs.python-requests.org)
+* BeautifulSoup4
+
+
+## Setup   
+
+Clone this repository first, then clone the hs-card-tiles repo [https://github.com/HearthSim/hs-card-tiles](https://github.com/HearthSim/hs-card-tiles) within the `DeckClusterer` directory.   
+
+```
+git clone https://github.com/HearthSim/hs-card-tiles
+```
+
+Then properly install a Python version of atleast 3.8   
+Then at the **first install, and after every Hearthstone Patch, Card Change, Or addition of Hero Portrait/Cardback**   
+run `setup.py` found in the `setup` directory.
+
+```
+cd setup   
+python setup.py    
+```
+
+This will update the card database, hearthstone library, and pull the most recent card tiles.   
+
+### USAGE
+
+To run the Clustering or Classification Program
+```
+python main.py
+```   
+and follow instructions on the GUI   
+   
+To Generate CSVs from Battlefy Data:    
+```
+cd CSVs/   
+python getCSVsThreaded
